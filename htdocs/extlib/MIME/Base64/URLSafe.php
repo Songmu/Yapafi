269ce5fb5d0c76a317f2941c6b841f30c0280f98
Songmu <y.songmu@gmail.com>
2010-12-07 16:45:00 +0900

    関数追加

--- htdocs/extlib/MIME/Base64/URLSafe.php
+++ htdocs/extlib/MIME/Base64/URLSafe.php
@@ -4,21 +4,30 @@ class MIME_Base64_URLSafe{
     
     static function encode($str){
         $str = base64_encode($str);
-        $str = str_replace('+', '-', $str);
+        return self::encodeBase64toURLSafe($str);
+    }
+    
+    static function decode($str){
+        $str = self::restructBase64fromURLSafe($str);
+        return base64_decode($str);
+    }
+    
+    static function encodeBase64toURLSafe($normal_base64){
+        $str = str_replace('+', '-', $normal_base64);
         $str = str_replace('/', '_', $str);
         $str = str_replace('=', '', $str);
         return $str;
     }
     
-    static function decode($str){
-        $str  = preg_replace('![\t-\x0d ]!', '', $str);
+    static function restructBase64fromURLSafe($url_safe_base64){
+        $str  = preg_replace('![\t-\x0d ]!', '', $url_safe_base64);
         $str  = str_replace('-', '+', $str);
         $str  = str_replace('_', '/', $str);
         $mod4 = strlen($str) % 4;
         if ( $mod4 ){
             $str .= substr('====', $mod4);
         }
-        return base64_decode($str);
+        return $str;
     }
     
 }
